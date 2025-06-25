cd3cf1d0a84c3c9e0c3b8152852edaed1a3845e5
Your Name <you@example.com>
2025-06-25 21:28:21 +0300

    changed user route to moderators route , implemented revoke permissions controller

--- routes/moderatorRoute.js
+++ routes/moderatorRoute.js
@@ -3,7 +3,9 @@ const adminController = require('../controllers/adminController');
 const protection = require('../middlewares/protectionMiddleware');
 const restriction = require('../middlewares/restrictionMiddleware');
 
-router.route('/:id').patch(protection, restriction(['admin']), adminController.assignPermissions);
+router.route('/:id/assign-permissions').patch(protection, restriction(['admin']), adminController.assignPermissions);
+router.route('/:id/revoke-permissions').patch(protection, restriction(['admin']), adminController.revokePermission);
+
 router.route('/').post(protection, restriction(['admin']), adminController.createAdminsAndModerators);
 
 module.exports = router;
