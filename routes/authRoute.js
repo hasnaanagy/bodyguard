3244e727ecd7acaf57b55e3fe6e9648beb872d44
alisaafan <Aligithub@yahoo.com>
2025-06-25 15:16:38 +0300

    update auth route set protection

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const verifyToken = require('../middlewares/verifyToken');
 const protection = require('../middlewares/protectionMiddleware');
 const restriction = require('../middlewares/restrictionMiddleware');
 const { uploadMultiple } = require('../middlewares/upload');
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const reviewController = require('../controllers/reviewController');
-const verifyToken = require('../middlewares/verifyToken'); // لو عندك middleware حماية
-
+const protection = require('../middlewares/protectionMiddleware');
+const restriction = require('../middlewares/restrictionMiddleware');
 const router = express.Router();
 
 // Public routes
-router.get('/', reviewController.getAllReviews);
+router.get('/', protection, restriction('admin', 'moderator'), reviewController.getAllReviews);
 router.get('/guard/:guardId', reviewController.getReviewsByGuard);
 
 // Protected route
-router.post('/:id', verifyToken, reviewController.createReview);
+router.post('/:id', protection, reviewController.createReview);
 
 module.exports = router;
